6cc3f2b7acbcac94af3482474a2127ea8f7db9e6
sir.gippo <ibodrov3@gmail.com>
2024-05-27 18:49:47 +0200

    Add query client

--- src/providers/AppQueryClientProvider.tsx
+++ src/providers/AppQueryClientProvider.tsx
@@ -1,12 +1,25 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactNode } from "@tanstack/react-router";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const SECOND = 1000;
+const MINUTE = SECOND * 60;
 
 // Create a client
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 10 * MINUTE,
+    },
+  },
+});
 
 export function AppQueryClientProvider({ children }: { children: ReactNode }) {
   // Provide the client to your App
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,50 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { useQuery } from "@tanstack/react-query";
+import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/')({
-  component: () => <div>Hello /!</div>
-})
+type MonstersReturnType = {
+  count: number;
+  results: {
+    index: string;
+    name: string;
+    url: string;
+  }[];
+};
+
+export const Route = createFileRoute("/")({
+  component: () => {
+    const monsters = useQuery({
+      queryKey: ["monsters"],
+      queryFn: async (): Promise<MonstersReturnType> => {
+        const response = await fetch("https://www.dnd5eapi.co/api/monsters");
+        return response.json();
+      },
+    });
+
+    const { data, status, refetch } = monsters;
+
+    console.log(status, data);
+
+    return (
+      <>
+        <div>Hello /!</div>
+        {status === "error" && (
+          <>
+            <div>Error!</div>
+            <button onClick={() => refetch()}>Retry</button>
+          </>
+        )}
+
+        {status === "pending" && <div>Loading...</div>}
+
+        {status === "success" && (
+          <div>
+            There are {data.count} monsters
+            {data.results.map((monster) => (
+              <div>{monster.name}</div>
+            ))}
+          </div>
+        )}
+      </>
+    );
+  },
+});
